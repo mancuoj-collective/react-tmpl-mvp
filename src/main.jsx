d1b5e2db0588e4a8f3742c88c8406baf6a439171
mancuoj <mancuoj@gmail.com>
2024-02-17 16:00:17 +0800

    chore: nr format

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <h1 className="font-serif text-4xl">Hello React</h1>
-  </React.StrictMode>
+  </React.StrictMode>,
 )
