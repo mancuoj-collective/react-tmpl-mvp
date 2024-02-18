fdd16b5dbc16eb986cd686e39b6901be2c7b13eb
mancuoj <mancuoj@gmail.com>
2024-02-19 00:36:04 +0800

    chore: update

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,18 @@
 export default function App() {
   return (
-    <h1 className="flex items-center gap-5 font-serif text-4xl">
-      React MVP
-      <label className="swap swap-flip">
-        <input type="checkbox" className="hidden" />
-        <div className="swap-on">😈</div>
-        <div className="swap-off">😇</div>
-      </label>
-    </h1>
+    <div className="flex h-dvh items-center justify-center">
+      <h1 className="flex items-center gap-5 font-serif text-4xl">
+        React MVP
+        <label className="swap swap-flip">
+          <input type="checkbox" className="hidden" />
+          <div className="swap-on flex items-center justify-center">
+            <span className="i-lucide-heart-off"></span>
+          </div>
+          <div className="swap-off flex items-center justify-center">
+            <span className="i-lucide-heart"></span>
+          </div>
+        </label>
+      </h1>
+    </div>
   )
 }
