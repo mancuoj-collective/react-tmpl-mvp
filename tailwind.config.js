66cca5392877507d1fd3addf7f0d0b0a5b1fca7b
mancuoj <mancuoj@gmail.com>
2024-03-10 02:14:03 +0800

    chore: update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
-const { iconsPlugin, getIconCollections } = require("@egoist/tailwindcss-icons")
+const { iconsPlugin, getIconCollections } = require('@egoist/tailwindcss-icons')
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [
     iconsPlugin({
-      collections: getIconCollections(["lucide"]),
+      collections: getIconCollections(['lucide']),
     }),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/container-queries"),
-    require("daisyui"),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+    require('daisyui'),
   ],
   daisyui: {
-    themes: ["lemonade", "dim"],
-    darkTheme: "dim",
+    themes: ['lemonade', 'dim'],
+    darkTheme: 'dim',
   },
 }
