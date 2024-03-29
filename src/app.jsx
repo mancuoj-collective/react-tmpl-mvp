67c04298d4b1fa4984b7cbf62a93318836a3a5d7
mancuoj <mancuoj@gmail.com>
2024-03-29 21:59:17 +0800

    chore: update deps

--- src/app.jsx
+++ src/app.jsx
@@ -1,18 +1,7 @@
 export default function App() {
   return (
-    <div className="flex h-dvh items-center justify-center">
-      <h1 className="flex items-center gap-5 font-serif text-4xl">
-        React MVP
-        <label className="swap swap-flip">
-          <input type="checkbox" className="hidden" />
-          <div className="swap-on flex items-center justify-center">
-            <span className="i-lucide-heart-off"></span>
-          </div>
-          <div className="swap-off flex items-center justify-center">
-            <span className="i-lucide-heart"></span>
-          </div>
-        </label>
-      </h1>
+    <div className="flex h-dvh items-center justify-center font-serif">
+      <h1 className="text-4xl">React MVP</h1>
     </div>
   )
 }
