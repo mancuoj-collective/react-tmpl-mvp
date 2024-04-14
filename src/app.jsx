2702d6b97c8bc09071d976ace4b35640934e73cf
mancuoj <mancuoj@gmail.com>
2024-04-14 19:42:06 +0800

    style: update

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,11 @@ import { ThemeToggle } from './components/theme-toggle'
 
 export default function App() {
   return (
-    <div className="flex h-dvh items-center justify-center gap-5 font-serif">
-      <h1 className="text-3xl font-bold">React MVP</h1>
-      <ThemeToggle />
+    <div className="flex min-h-dvh font-sans antialiased">
+      <div className="container mx-auto flex items-center justify-center gap-5">
+        <h1 className="font-serif text-3xl font-bold">React MVP</h1>
+        <ThemeToggle />
+      </div>
     </div>
   )
 }
